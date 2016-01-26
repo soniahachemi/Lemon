65e5f30da2a1378721f2e1b1889625a7c55c9eb4
Sonia Hachemi <sonia.hachemi@etudiant.univ-lille1.fr>
2016-01-26 11:21:26 +0100

    creation de la socket

--- webserver/main.c
+++ webserver/main.c
@@ -2,22 +2,15 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include "socket.h"
 
-int main ( int argc , char ** argv )
+int main (void)
 {
-/* Arnold Robbins in the LJ of February ’95 , describing RCS */
-	if(argc > 1 && strcmp(argv [1]," - advice ")== 0){
-		printf("Dont panick");
-		return 42;
-	}
-	printf("need an advice ?");
 	int socket_serveur;
-	socket_serveur= socket(AF_INET, SOCK_STREAM,0);
+	socket_serveur= creer_serveur(8080);
 	if(socket_serveur==-1){
 		perror("socket_serveur");
 		return -1;
 	}
-
-
 	return 0;
 }
--- webserver/socket.c
+++ webserver/socket.c
@@ -2,10 +2,11 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 
 int creer_serveur(int port){
   int socket_serveur ;
-  socket_serveur = socket ( AF_INET , SOCK_STREAM , 0);
+  socket_serveur = socket( AF_INET , SOCK_STREAM , 0);
   if ( socket_serveur == -1)
     {
       perror ( " socket_serveur " );
@@ -13,13 +14,14 @@ int creer_serveur(int port){
     }
   /* Utilisation de la socket serveur */
 
-  struct sockaddr_in saddr ;
-saddr . sin_family = AF_INET ; /* Socket ipv4 */
-saddr . sin_port = htons (8080); /* Port d ’ écoute */
-saddr . sin_addr . s_addr = INADDR_ANY ; /* écoute sur toutes les interfaces */
-if ( bind ( socket_serveur , ( struct sockaddr *)& saddr , sizeof ( saddr )) == -1)
-{
-perror ( " bind socker_serveur " );
-/* traitement de l ’ erreur */
-}
+ 	 struct sockaddr_in saddr ;
+	saddr.sin_family = AF_INET ; /* Socket ipv4 */
+	saddr.sin_port = htons(port); /* Port d ’ écoute */
+	saddr.sin_addr.s_addr = INADDR_ANY ; /* écoute sur toutes les interfaces */
+	if(bind ( socket_serveur , ( struct sockaddr *)& saddr , sizeof( saddr )) == -1)
+	{
+		perror( " bind socker_serveur " );
+	/* traitement de l ’ erreur */
+	}
+return socket_serveur;
 }
