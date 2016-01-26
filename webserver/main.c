cd39933906412f938e7c16d673a64b955e91707b
Sonia Hachemi <sonia.hachemi@etudiant.univ-lille1.fr>
2016-01-26 10:58:10 +0100

    modification main

--- webserver/main.c
+++ webserver/main.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
 int main ( int argc , char ** argv )
 {
 /* Arnold Robbins in the LJ of February ’95 , describing RCS */
-if(argc > 1 && strcmp(argv [1]," - advice ")== 0){
-printf("Dont panick");
-return 42;
-}
-printf("need an advice ?");
-return 0;
+	if(argc > 1 && strcmp(argv [1]," - advice ")== 0){
+		printf("Dont panick");
+		return 42;
+	}
+	printf("need an advice ?");
+	int socket_serveur;
+	socket_serveur= socket(AF_INET, SOCK_STREAM,0);
+	if(socket_serveur==-1){
+		perror("socket_serveur");
+		return -1;
+	}
+
+
+	return 0;
 }
