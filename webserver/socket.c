ad2577b40fe9c3db9027436bf3b470fd9eaa5ede
cottona <alex59219@sfr.fr>
2016-01-26 11:11:16 +0100

    Ajout de socket.c

---
+++ webserver/socket.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+int creer_serveur(int port){
+  int socket_serveur ;
+  socket_serveur = socket ( AF_INET , SOCK_STREAM , 0);
+  if ( socket_serveur == -1)
+    {
+      perror ( " socket_serveur " );
+      /* traitement de l ’ erreur */
+    }
+  /* Utilisation de la socket serveur */
+
+  struct sockaddr_in saddr ;
+saddr . sin_family = AF_INET ; /* Socket ipv4 */
+saddr . sin_port = htons (8080); /* Port d ’ écoute */
+saddr . sin_addr . s_addr = INADDR_ANY ; /* écoute sur toutes les interfaces */
+if ( bind ( socket_serveur , ( struct sockaddr *)& saddr , sizeof ( saddr )) == -1)
+{
+perror ( " bind socker_serveur " );
+/* traitement de l ’ erreur */
+}
+}
