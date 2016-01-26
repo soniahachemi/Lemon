fcb61a3521129e29c12f39908d7fc31f86a095b8
Sonia Hachemi <sonia.hachemi@etudiant.univ-lille1.fr>
2016-01-26 11:44:18 +0100

    creation d'une socket client dans le main

--- webserver/main.c
+++ webserver/main.c
@@ -13,6 +13,16 @@ int main (void)
 		perror("socket_serveur");
 		return -1;
 	}
+// Creation client
+	int socket_client ;
+	socket_client = accept ( socket_serveur , NULL , NULL );
+	if ( socket_client == -1)
+	{
+		perror ( " accept " );
+		/* traitement d ’ erreur */
+	}
+	const char * message_bienvenue="bjr";
+	write ( socket_client , message_bienvenue , strlen ( message_bienvenue ));
 	close(socket_serveur);
 	return 0;
 }
