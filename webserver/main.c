281065ea091ad4e803f59e48d7de59b59f94e7a6
Sonia Hachemi <sonia.hachemi@etudiant.univ-lille1.fr>
2016-02-02 10:07:36 +0100

    correction serveur, le serveur ne se ferme plus apres une connexion

--- webserver/main.c
+++ webserver/main.c
@@ -4,10 +4,17 @@
 #include <sys/socket.h>
 #include "socket.h"
 #include <unistd.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
 
 int main (void)
 {
+
+	struct sockaddr_in clientname;
 	int socket_serveur;
+	socklen_t size;
 	socket_serveur= creer_serveur(8080);
 	if(socket_serveur==-1){
 		perror("socket_serveur");
@@ -15,14 +22,18 @@ int main (void)
 	}
 // Creation client
 	int socket_client ;
-	socket_client = accept ( socket_serveur , NULL , NULL );
+	size = sizeof (clientname);
+	while(1){
+	socket_client = accept ( socket_serveur , (struct sockaddr *) &clientname, &size);
 	if ( socket_client == -1)
 	{
 		perror ( " accept " );
 		/* traitement d ’ erreur */
 	}
 	const char * message_bienvenue="bjr";
-	write ( socket_client , message_bienvenue , strlen ( message_bienvenue ));
+		write ( socket_client , message_bienvenue , strlen ( message_bienvenue ));
+		
+	}
 	close(socket_serveur);
 	return 0;
 }
